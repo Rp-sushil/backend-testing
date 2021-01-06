dc9c5bee1d5f99a5c484098872039dd84ea92e9a
Rp-sushil <sushil.18je0861@am.iitism.ac.in>
2021-01-07 02:17:47 +0530

    error fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,23 @@
 const expect = require("chai").expect;
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://mongoService:27017/" + "softHardTesting";
+
+before(done, () => {
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+  mongoose.connection
+    .once("open", () => {
+      console.log("connected to database");
+      done();
+    })
+    .on("connectionError", (err) => {
+      console.log(err);
+    });
+});
 
 describe("Test-1", () => {
   it("Hello testing-1", () => {
